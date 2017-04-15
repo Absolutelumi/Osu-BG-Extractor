cedfee553aaf272577a2d2cabf009407297de85b
Jackson Saylor <jackson.saylor@yahoo.com>
2017-04-15 18:43:41 -0400

    Test

---
+++ osu!bg-extractor/osu!bg-extractor/Osu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu_bg_extractor
+{
+    class Osu
+    {
+        private readonly string OsuSongPath = "fuck"; 
+
+        public string FindBG(string BeatmapName)
+        {
+            
+        }
+    }
+}
